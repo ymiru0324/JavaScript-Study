b8b7df5aa135bc2b46309cd66d33f76cd9448cdb
ymiru0324 <ymiru0324@naver.com>
2022-05-12 01:22:11 +0900

    [220512] js-study

--- js/tab.js
+++ js/tab.js
@@ -1,11 +1,19 @@
 const tb = $('.tab-button');
 const tc = $('.tab-content');
 
-for(let i = 0; i < tb.length; i++) {
-  tb.eq(i).on('click', function() {
-    tb.removeClass('orange');
-    tb.eq(i).addClass('orange');
-    tc.removeClass('show');
-    tc.eq(i).addClass('show');
-  });
-}
+// for(let i = 0; i < tb.length; i++) {
+//   tb.eq(i).on('click', function() {
+//     tapOpen(i)
+//   });
+// }
+
+$('.list').click(function(e) {
+    tabOpen(e.target.dataset.id);
+})
+
+function tabOpen(index) {
+  tb.removeClass('orange');
+  tb.eq(index).addClass('orange');
+  tc.removeClass('show');
+  tc.eq(index).addClass('show');
+};
